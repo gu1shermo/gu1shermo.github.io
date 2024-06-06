365d36112f60206ab29493ca88a003b804cf0f05
gcournet <cournet.guillaume@protonmail.com>
2024-06-06 21:49:39 +0200

    test load content

--- add_menu.js
+++ add_menu.js
@@ -4,7 +4,7 @@ htmlContent+= "<ul class=\"menu\">";
 htmlContent+= "<li><a href=\"./index.html\">Accueil</a></li>";
 htmlContent+= "<li><a href=\"#about\">À propos</a></li>";
 htmlContent+= "<li><a href=\"./memoire/index.html\">Mémoire</a></li>";
-htmlContent+= "<li><a href=\"#test\">Test</a></li>";
+htmlContent+= "<li id=\"loadContentButton\"><a href=\"#test\">Test</a></li>";
 htmlContent+= "<li><a href=\"#contact\">Contact</a></li>";
 htmlContent+= "</ul>";
 htmlContent+= "</nav>";

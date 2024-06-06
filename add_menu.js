c9cb859774d3427093ae4c35e837a2c40b64cc78
gcournet <cournet.guillaume@protonmail.com>
2024-06-06 10:35:53 +0200

    test cors policy

---
+++ add_menu.js
@@ -0,0 +1,11 @@
+// JavaScript code to include HTML content
+var htmlContent = "<nav>";
+htmlContent+= "<ul class=\"menu\">";
+htmlContent+= "<li><a href=\"./index.html\">Accueil</a></li>";
+htmlContent+= "<li><a href=\"#about\">À propos</a></li>";
+htmlContent+= "<li><a href=\"./memoire/index.html\">Mémoire</a></li>";
+htmlContent+= "<li><a href=\"#test\">Test</a></li>";
+htmlContent+= "<li><a href=\"#contact\">Contact</a></li>";
+htmlContent+= "</ul>";
+htmlContent+= "</nav>";
+document.getElementById('content').innerHTML = htmlContent;

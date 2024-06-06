40dcb8b71147771d78da58831b6f4e0a3836b20a
gcournet <cournet.guillaume@protonmail.com>
2024-06-06 10:50:12 +0200

    test load content

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('loadContentButton').addEventListener('click', function() {
+    fetch('content.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(html => {
+            document.getElementById('main_content').innerHTML = html;
+        })
+        .catch(error => {
+            console.error('Error fetching content:', error);
+        });
+});

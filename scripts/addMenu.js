abaa6e4e575821b39f1de90d51f546a440668686
gcournet <cournet.guillaume@protonmail.com>
2024-06-07 23:25:11 +0200

    clean

--- scripts/addMenu.js
+++ scripts/addMenu.js
@@ -1,9 +1,8 @@
 // JavaScript code to include HTML content
 var htmlContent = "<nav>";
 htmlContent+= "<ul class=\"menu\">";
-htmlContent+= "<li><a href=\"./index.html\">Accueil</a></li>";
 htmlContent+= "<li><a href=\"./memoire/index.html\" target=\"_blank\">Mémoire</a></li>";
-htmlContent+= "<li><a href=\"./contact/index.html\">Contact</a></li>";
+htmlContent+= "<li><a href=\"./contact/index.html\" target=\"_blank\">Contact</a></li>";
 htmlContent+= "</ul>";
 htmlContent+= "</nav>";
 document.getElementById('menu').innerHTML = htmlContent;

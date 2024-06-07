119a9f2ba070569b64eff4b580052ddae9ece91a
gcournet <cournet.guillaume@protonmail.com>
2024-06-07 19:26:32 +0200

    menu vertical

--- scripts/addMenu.js
+++ scripts/addMenu.js
@@ -2,9 +2,7 @@
 var htmlContent = "<nav>";
 htmlContent+= "<ul class=\"menu\">";
 htmlContent+= "<li><a href=\"./index.html\">Accueil</a></li>";
-htmlContent+= "<li><a href=\"#about\">À propos</a></li>";
 htmlContent+= "<li><a href=\"./memoire/index.html\" target=\"_blank\">Mémoire</a></li>";
-htmlContent+= "<li><a href=\"#test\">Test</a></li>";
 htmlContent+= "<li><a href=\"./contact/index.html\">Contact</a></li>";
 htmlContent+= "</ul>";
 htmlContent+= "</nav>";

a73199ee0e52d7f16dca88fcae1b4fb1f97bb2e7
gcournet <cournet.guillaume@protonmail.com>
2024-06-07 11:59:57 +0200

    debug menu color

--- scripts/addMenu.js
+++ scripts/addMenu.js
@@ -1,11 +1,11 @@
 // JavaScript code to include HTML content
 var htmlContent = "<nav>";
 htmlContent+= "<ul class=\"menu\">";
-htmlContent+= "<li><a href=\"./index.html\">Accueil</a></li>";
-htmlContent+= "<li><a href=\"#about\">À propos</a></li>";
-htmlContent+= "<li><a href=\"./memoire/index.html\">Mémoire</a></li>";
-htmlContent+= "<li id=\"loadContentButton\"><a href=\"#test\">Test</a></li>";
-htmlContent+= "<li><a href=\"#contact\">Contact</a></li>";
+htmlContent+= "<li class=\"menu\"><a href=\"./index.html\">Accueil</a></li>";
+htmlContent+= "<li class=\"menu\"><a href=\"#about\">À propos</a></li>";
+htmlContent+= "<li class=\"menu\"><a href=\"./memoire/index.html\">Mémoire</a></li>";
+htmlContent+= "<li class=\"menu\" id=\"loadContentButton\"><a href=\"#test\">Test</a></li>";
+htmlContent+= "<li class=\"menu\"><a href=\"#contact\">Contact</a></li>";
 htmlContent+= "</ul>";
 htmlContent+= "</nav>";
 document.getElementById('menu').innerHTML = htmlContent;

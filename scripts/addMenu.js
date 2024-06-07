fa25ee053333fa1257b7727a9dc456a127f9c116
gcournet <cournet.guillaume@protonmail.com>
2024-06-07 17:52:49 +0200

    get root folder

--- scripts/addMenu.js
+++ scripts/addMenu.js
@@ -1,11 +1,22 @@
+function getRootFolder() {
+    const protocol = window.location.protocol;
+    const host = window.location.host;
+    const rootFolder = `${protocol}//${host}/`;
+    return rootFolder;
+}
+
+// Usage
+const rootFolder = getRootFolder();
+console.log("Root folder:", rootFolder);
+
 // JavaScript code to include HTML content
 var htmlContent = "<nav>";
 htmlContent+= "<ul class=\"menu\">";
-htmlContent+= "<li class=\"menu\"><a href=\"./index.html\">Accueil</a></li>";
-htmlContent+= "<li class=\"menu\"><a href=\"#about\">À propos</a></li>";
-htmlContent+= "<li class=\"menu\"><a href=\"./memoire/index.html\">Mémoire</a></li>";
-htmlContent+= "<li class=\"menu\" id=\"loadContentButton\"><a href=\"#test\">Test</a></li>";
-htmlContent+= "<li class=\"menu\"><a href=\"#contact\">Contact</a></li>";
+htmlContent+= "<li><a href=\"./index.html\">Accueil</a></li>";
+htmlContent+= "<li><a href=\"#about\">À propos</a></li>";
+htmlContent+= "<li><a href=\"./memoire/index.html\">Mémoire</a></li>";
+htmlContent+= "<li><a href=\"#test\">Test</a></li>";
+htmlContent+= "<li><a href=\"#contact\">Contact</a></li>";
 htmlContent+= "</ul>";
 htmlContent+= "</nav>";
 document.getElementById('menu').innerHTML = htmlContent;

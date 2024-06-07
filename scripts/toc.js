3ed97f0d9d8d489c6d01489d6e325d54809e7cf0
gcournet <cournet.guillaume@protonmail.com>
2024-06-07 17:13:57 +0200

    new toc

--- scripts/toc.js
+++ scripts/toc.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    alert("alert(toc.js)");
     const tocContent = document.getElementById('toc-content');
     const headers = document.querySelectorAll('h1, h2, h3');
     headers.forEach((header, index) => {
@@ -11,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
         link.classList.add(`toc-${header.tagName.toLowerCase()}`);
         tocContent.appendChild(link);
     });
-
     // Assurez-vous que la case est décochée au chargement de la page
     document.getElementById('toc-toggle').checked = false;
 });
+

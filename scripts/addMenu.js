6f531d149ef55ec0963420b0c16bd90d2c78c671
gu1shermo <61197055+gu1shermo@users.noreply.github.com>
2024-08-06 09:18:08 +0200

    change name to orca

--- scripts/addMenu.js
+++ scripts/addMenu.js
@@ -2,7 +2,7 @@
 var htmlContent = "<nav>";
 htmlContent+= "<ul class=\"menu\">";
 htmlContent+= "<li><a href=\"./memoire/index.html\" target=\"_blank\">Mémoire</a></li>";
-htmlContent+= "<li><a href=\"./orca/index.html\" target=\"_blank\">Mémoire</a></li>";
+htmlContent+= "<li><a href=\"./orca/index.html\" target=\"_blank\">Orca</a></li>";
 htmlContent+= "<li><a href=\"./contact/index.html\" target=\"_blank\">Contact</a></li>";
 htmlContent+= "</ul>";
 htmlContent+= "</nav>";

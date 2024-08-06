66cb3b458c07cab035a6ef2c8b5cc57b58b8df24
gu1shermo <61197055+gu1shermo@users.noreply.github.com>
2024-08-06 11:30:56 +0200

    dynamic footer

---
+++ scripts/addFooter.js
@@ -0,0 +1,5 @@
+// JavaScript code to include HTML content
+var htmlContent = "<div class=\"container\">";
+htmlContent+= "<p>&copy; Guillaume Cournet. All rights reserved.</p>";
+htmlContent+= "</div>";
+document.getElementById('footer').innerHTML = htmlContent;
